db6bcc90452875bdc9df666af8ea88c9297dafe7
alexwittwer <wittweral@gmail.com>
2023-11-13 18:30:20 -0500

    add nav

--- src/input.js
+++ src/input.js
@@ -1,9 +1,10 @@
 import "./input.css";
 import makeTitle from "./modules/title";
 import makeHeader from "./modules/header";
+import makeNav from "./modules/nav";
 
 const container = document.querySelector(".container");
 
 container.appendChild(makeTitle());
-
 container.appendChild(makeHeader());
+container.appendChild(makeNav());

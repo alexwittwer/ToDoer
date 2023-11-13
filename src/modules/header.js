bf82663779fe39dcb927f273f49fc5d31f7c492f
alexwittwer <wittweral@gmail.com>
2023-11-13 17:05:53 -0500

    add header

---
+++ src/modules/header.js
@@ -0,0 +1,19 @@
+import Logo from "../assets/todo-title.svg";
+
+export default function makeHeader() {
+  const header = document.createElement("header");
+  const title = document.createElement("h3");
+  const logo = new Image();
+
+  title.textContent = "ToDoer //";
+
+  logo.src = Logo;
+  logo.classList.add("logo");
+  header.classList.add("section-header");
+  title.classList.add("app-title");
+
+  header.appendChild(title);
+  header.appendChild(logo);
+
+  return header;
+}

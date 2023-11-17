18851e4319da2d6063efb332849d82b0f507d16a
alexwittwer <wittweral@gmail.com>
2023-11-16 21:03:05 -0500

    add semi-functional todo and project tracking

--- src/input.js
+++ src/input.js
@@ -5,29 +5,37 @@ import {
   Header,
   Title,
   ProjectModal,
-  TodoModal,
+  renderProject,
 } from "./modules/TDRender";
-import { Todo, Project, addProject } from "./modules/todo";
-
-const projects = [];
-
+import { Todo, Project } from "./modules/todo";
+// html container
 const container = document.querySelector(".container");
+const todoContainer = Content();
 
-// initial population
-container.appendChild(ProjectModal());
-container.appendChild(TodoModal());
+// project storage
+const projects = [];
+const currentProject = null;
+
+// populate page
+container.appendChild(ProjectModal(projects));
 container.appendChild(Title());
 container.appendChild(Header());
 container.appendChild(Nav());
-const todoContainer = Content();
+
+// initial population
 
 // testing
 const project_1 = new Project("New Project");
-projects.push(project_1);
 const todo_1 = new Todo();
 project_1.add(todo_1);
-addProject(project_1, todoContainer);
+renderProject(project_1, todoContainer);
 
 console.log(projects);
 
 container.appendChild(todoContainer);
+
+const testbutton = document.createElement("button");
+testbutton.addEventListener("click", () => {
+  console.log(projects);
+});
+container.appendChild(testbutton);
--- src/modules/TDRender.js
+++ src/modules/TDRender.js
@@ -1,15 +1,47 @@
-import { Project, Todo, addTodo } from "./todo";
+import { Project, Todo } from "./todo";
 import Logo from "../assets/todo-title.svg";
 
-export function Content(projectArg) {
-  if (!document.querySelector(".content-section")) {
-    const content = document.createElement("section");
-    content.classList.add("content-section");
+// renders a new Todoer item, local function only
+function renderTodo(todo_arg) {
+  const todoElem = document.createElement("div");
+  const titleElem = document.createElement("div");
+  const descriptionElem = document.createElement("div");
+  const priorityElem = document.createElement("div");
+  const dueElem = document.createElement("div");
+  const completedElem = document.createElement("input");
+  const completedElemLabel = document.createElement("label");
+  const id = crypto.randomUUID();
 
-    return content;
-  }
+  completedElem.type = "checkbox";
+  completedElem.id = id;
+  completedElemLabel.setAttribute("for", id);
+  completedElemLabel.textContent = "Mark Complete: ";
+  completedElemLabel.appendChild(completedElem);
+
+  todoElem.classList.add("todo-item");
+
+  titleElem.textContent = todo_arg.title;
+  descriptionElem.textContent = todo_arg.description;
+  priorityElem.textContent = todo_arg.priority;
+  dueElem.textContent = todo_arg.due;
+  completedElem.checked = todo_arg.completed;
+
+  const todo_elements = [
+    titleElem,
+    descriptionElem,
+    priorityElem,
+    dueElem,
+    completedElemLabel,
+  ];
+  todo_elements.forEach((e) => {
+    e.classList.add("todo-param");
+    todoElem.appendChild(e);
+  });
+
+  return todoElem;
 }
 
+// updates the content section
 export function updateContent(project) {
   const container = document.querySelector(".content-section"); // content section container
   // kills all children
@@ -27,46 +59,18 @@ export function updateContent(project) {
   // appends all the todos from the project, if any.
   if (project.todos === undefined) {
     // creates the button for adding todos from modal
+    return;
+  } else {
+    project.todos.forEach((item) => {
+      container.appendChild(renderTodo(item));
+    });
   }
+
+  return;
 }
 
-export function Title() {
-  const container = document.createElement("div");
-  const title_1 = document.createElement("h1");
-  const title_2 = document.createElement("h1");
-
-  container.classList.add("section-container-title");
-  title_1.classList.add("title", "title-one");
-  title_2.classList.add("title", "title-two");
-
-  title_1.textContent = "To";
-  title_2.textContent = "Doer//";
-
-  container.appendChild(title_1);
-  container.appendChild(title_2);
-
-  return container;
-}
-
-export function Header() {
-  const header = document.createElement("header");
-  const title = document.createElement("h3");
-  const logo = new Image();
-
-  title.textContent = "ToDoer //";
-
-  logo.src = Logo;
-  logo.classList.add("logo");
-  header.classList.add("section-header");
-  title.classList.add("app-title");
-
-  header.appendChild(title);
-  header.appendChild(logo);
-
-  return header;
-}
-
-export function TodoModal() {
+// Renders the Todo Modal pane (accessible from projects only)
+export function TodoModal(currentProject) {
   const form = document.createElement("form");
 
   const titleLabel = document.createElement("label");
@@ -117,11 +121,13 @@ export function TodoModal() {
       priorityInput.checked,
       dueInput.value
     );
-    // add todo to current project
+    // update content
     const container = document.querySelector(".content-section");
     const modal = document.querySelector(".todo-modal");
-    container.appendChild(addTodo(newTodo));
+    container.appendChild(renderTodo(newTodo));
     modal.classList.toggle("hidden");
+    currentProject.add(newTodo);
+    console.log(currentProject);
   });
 
   form.appendChild(titleLabel);
@@ -133,7 +139,21 @@ export function TodoModal() {
   return form;
 }
 
-export function ProjectModal() {
+// Creates a Project dom element and renders its todos
+export function renderProject(projects, container, projectsArr) {
+  if (projects.todos == undefined) {
+    const div = document.createElement("div");
+    div.textContent = projects.project;
+    container.appendChild(div);
+  } else {
+    projects.todos.forEach((todo) => {
+      container.appendChild(renderTodo(todo));
+    });
+  }
+}
+
+// creates the Project modal for creating new projects
+export function ProjectModal(projectsArr) {
   const form = document.createElement("form");
   const titleLabel = document.createElement("label");
   const titleInput = document.createElement("input");
@@ -153,17 +173,33 @@ export function ProjectModal() {
     // toggle hidden
     form.classList.toggle("hidden");
 
+    //clear project-nav
+    const projects = document.querySelector(".projects-nav");
+    if (projects.firstElementChild) {
+      while (projects.firstElementChild) {
+        projects.removeChild(projects.firstElementChild);
+      }
+    }
+
     // creates new project
     const newProject = new Project(titleInput.value);
 
     // creates link to project in nav
-    const projects = document.querySelector(".projects-nav");
-    const newProjectElem = document.createElement("div");
-    newProjectElem.addEventListener("click", (e) => {
-      updateContent(newProject);
+    projectsArr.push(newProject); // updates the index with the new project
+    projectsArr.forEach((item) => {
+      // container for renderProjects
+      const newProjectElem = document.createElement("div"); // new clickable div (put in renderProjects), do foreach
+      newProjectElem.addEventListener("click", (e) => {
+        // this should update the content box with each of the projects todos
+        updateContent(item); // entry point is that project from the array
+        newProjectElem.textContent = item.project; // set text content for clickable nav item as the project title
+      });
+      newProjectElem.textContent = `${newProject.project}`;
+      projects.appendChild(newProjectElem); // appends the content
     });
-    newProjectElem.textContent = newProject.project;
-    projects.appendChild(newProjectElem);
+    console.log(projectsArr);
+
+    document.body.appendChild(TodoModal(newProject));
   });
 
   titleLabel.setAttribute("for", "projectTitle");
@@ -176,6 +212,7 @@ export function ProjectModal() {
   return form;
 }
 
+// Renders the navigation pane
 export function Nav() {
   const nav = document.createElement("section");
   const thisWeek = document.createElement("nav");
@@ -209,4 +246,51 @@ export function Nav() {
   });
 
   return nav;
+}
+
+// Renders the title screen
+export function Title() {
+  const container = document.createElement("div");
+  const title_1 = document.createElement("h1");
+  const title_2 = document.createElement("h1");
+
+  container.classList.add("section-container-title");
+  title_1.classList.add("title", "title-one");
+  title_2.classList.add("title", "title-two");
+
+  title_1.textContent = "To";
+  title_2.textContent = "Doer//";
+
+  container.appendChild(title_1);
+  container.appendChild(title_2);
+
+  return container;
+}
+
+// Renders the header
+export function Header() {
+  const header = document.createElement("header");
+  const title = document.createElement("h3");
+  const logo = new Image();
+
+  title.textContent = "ToDoer //";
+
+  logo.src = Logo;
+  logo.classList.add("logo");
+  header.classList.add("section-header");
+  title.classList.add("app-title");
+
+  header.appendChild(title);
+  header.appendChild(logo);
+
+  return header;
+}
+
+export function Content(projectArg) {
+  if (!document.querySelector(".content-section")) {
+    const content = document.createElement("section");
+    content.classList.add("content-section");
+
+    return content;
+  }
 }
